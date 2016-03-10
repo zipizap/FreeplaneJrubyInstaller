4e72b80e3dcce8fbb1bec33248017246d0d9de33
zipizap <zipizap123@gmail.com>
2016-03-10 12:30:50 +0100

    show_ruby_version.rb is not needed with new FreeplaneJrubyInstaller.jar

--- addon_lab/zips/scripts/show_jruby_version.rb
+++
@@ -1,8 +0,0 @@
-Java::OrgFreeplaneCoreUiComponents::UITools.informationMessage <<EOL
-Jruby is running inside Freeplane!
-
-Java version:      #{java.lang.System.getProperty("java.version")}
-Freeplane version: #{Java::OrgFreeplaneCoreUtil::FreeplaneVersion.getVersion.toString} (#{Java::OrgFreeplaneCoreUtil::FreeplaneVersion.getVersion.getRevision})
-Jruby version:     #{RUBY_DESCRIPTION}
-Ruby version:      #{RUBY_VERSION}
-EOL
