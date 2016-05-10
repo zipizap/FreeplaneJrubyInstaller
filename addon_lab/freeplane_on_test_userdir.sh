655256029e3452bd6d8f7d79b2f5de6f026f14a8
zipizap <zipizap123@gmail.com>
2016-05-10 10:21:28 +0200

    added option to indicate a freeplane_user_dir

--- addon_lab/freeplane_on_test_userdir.sh
+++ addon_lab/freeplane_on_test_userdir.sh
@@ -1,2 +1,4 @@
-DISPLAY=:0.0 $HOME/freeplane/freeplane.sh -U"$PWD/../test_userdir" FreeplaneJrubyInstaller.mm
+FREEPLANE_USER_DIR="$PWD/../test_userdir"
+[[ $1 ]] && FREEPLANE_USER_DIR="$1"
+DISPLAY=:0.0 $HOME/freeplane/freeplane.sh -U"$FREEPLANE_USER_DIR" FreeplaneJrubyInstaller.mm
   #-U<userdir>: set the freeplane user config directory (default: /home/ub/.config/freeplane)
