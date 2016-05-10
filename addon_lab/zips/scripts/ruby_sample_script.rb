10d102b13cd3de17017490eda30f2ca2dd4bfeb5
zipizap <zipizap123@gmail.com>
2016-05-10 10:17:51 +0200

    small improvement

--- addon_lab/zips/scripts/ruby_sample_script.rb
+++ addon_lab/zips/scripts/ruby_sample_script.rb
@@ -116,5 +116,5 @@ require "freeplane_jruby_common_environment.rb"
 # change the text of node
 node.text="The current time is: #{Time.now.to_s}"
 
-## You can open a debug window at any point of the code: just put the following line there:
+## You can open a debug window at any point of the code: just put the following line there (without the #):
 # RubyLiveDebugger.open_debug_here(binding)
